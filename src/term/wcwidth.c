9750f04cf9d7e53b83b91a0e9e26fa0951cd6567
Lino Gamba <lgamba@student.42perpignan.fr>
2025-05-06 15:32:53 +0200

    Basic wcwidth

--- src/term/wcwidth.c
+++ src/term/wcwidth.c
@@ -11,3 +11,36 @@
 /* ************************************************************************** */
 #include <shell/shell.h>
 
+size_t
+	grapheme_width(
+	t_shell *shell,
+	const char *str,
+	int expensive,
+	int skip_ansi)
+{
+	t_u8_iterator	it;
+	int				len;
+	size_t			total;
+	
+	total = 0;
+	if (expensive) { /* TODO */ }
+	it = it_new((t_string){str, ft_strlen(str)});
+	it_next(&it);
+	while (it.codepoint.len)
+	{
+		if (skip_ansi && !ft_strncmp(it_substr(&it, 2).str, "\033[", 2))
+		{
+			it_advance(&it, 2);
+			while (it.codepoint.len && ((it.codepoint.str[0] >= '0'
+				&& it.codepoint.str[0] <= '9') || it.codepoint.str[0] == ';'))
+					it_next(&it);
+			it_next(&it);
+			continue ;
+		}
+		len = codepoint_width(it.str.str);
+		if (len > 0)
+			total += len;
+		it_next(&it);
+	}
+	return (total);
+}
